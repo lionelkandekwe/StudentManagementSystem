cea131ca5e4d6cabe14325cfda87f6a75fa66940
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2023-01-12 15:41:56 +0200

    Create access token

--- app/Routers/auth.py
+++ app/Routers/auth.py
@@ -1,19 +1,23 @@
 from fastapi import APIRouter,status,Depends,HTTPException,Response
 from sqlalchemy.orm import Session
-from .. import database,schemas,models,utils
+from .. import database,models,utils,OAuth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router=APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_credentials:schemas.UserLogin,db: Session = Depends(database.get_db)):
+def login(user_credentials:OAuth2PasswordRequestForm=Depends(),db: Session = Depends(database.get_db)):
    user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
    if not utils.verify_password(user_credentials.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
-   return{"TOKEN":"Example Token"}
+   #Create Token
+   access_token=OAuth2.create_access_token(data={"user_id": user.id})
+   #Return Token
+   return{"access_token": access_token, "token_type": "bearer"}
 
 
